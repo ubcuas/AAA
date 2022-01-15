5f8c52a5001a07128ff3a973f0043006b2d7d2c8
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-01-15 12:17:22 -0800

    Code for testing and setup for checking reroute

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-
 import json
-from flask import Flask  
+from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import json
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
-# some sort of caching
+# Obstacle caching
 interops = {}
 obstacles = []
 
-@app.route('/aaa', methods=['POST']) #only support posting
+@app.route('/aaa', methods=['GET','POST']) # Only support posting - keeping GET for testing
 def responseHandler():
-    if request.method == 'POST':
-        # get the interop data; assuming data is in json, no modifications done?
+    if request.method == 'GET': # 'POST' for final
+        # Get the interop data; assuming data is in json, no modifications done?
         # interop = #do stuff to parse back into objects 
-        interops[request.interop.id] = request.interop
+        #interops[request.interop.id] = request.interop
 
         obs_list = create_obstacle_list()
 
-        # check if need to reroute
+        # Check if need to reroute
         reroute = need_reroute(obs_list)
+
+        # Save obstacle list to caching after comparing with previous list
+        obstacles = obs_list
 
         # Flask 1.1.0 a view can directly return a Python dict and Flask will call jsonify automatically
         if reroute:
@@ -32,21 +34,35 @@ def responseHandler():
 def create_obstacle_list():
     obs_list = []
 
-    # some obstacle:
-    o = {'Latitude': 0, 'Longitude': 0, 'Radius': 0, 'Height': 0}
-
     # Grab the data from the test json file and add it to the objects list
     with open('test-uas-telem.json') as f:
         obs_list = json.load(f)
 
-    #obs_list.append(o)
-
+    # Print the list to see that it worked
     print(obs_list)
 
     return obs_list
 
 def need_reroute(obstacles):
     """ check if obstacles have changed enough that we require a reroute"""
-    """ returns a boolean"""
-    return
 
+    # No reroute needed if it is the first telemtry received
+    if obstacles == None:
+        return False
+    # Make a temporary list of active aircraft (other than our own)
+    temp_obs = []
+
+    for obs in obstacles:
+        if obs.inAir == "true":
+            temp_obs.append(obs)
+    
+    print(temp_obs)
+
+    # Compare telemetry of our aircraft to others
+
+    """ returns a boolean"""
+    return True
+
+if __name__ == "__main__":
+
+    app.run()
