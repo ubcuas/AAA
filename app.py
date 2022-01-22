4b289ea1c53a375e974014539ef1596314e633a0
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-01-22 14:25:24 -0800

    Implemented time elapsed, added test json

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dis import dis
 import json
 import datetime
-#from turtle import speed
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -9,6 +8,7 @@ from flask import json
 from math import sqrt
 from numpy import true_divide
 from utm import to_latlon, from_latlon
+import dateutil.parser
 
 app = Flask(__name__)
 
@@ -24,10 +24,18 @@ def responseHandler():
         # interop = #do stuff to parse back into objects 
         #interops[request.interop.id] = request.interop
 
-        obs_list = create_obstacle_list(request.data)
+        obs_list = create_obstacle_list() # add back "request.data"
 
         # Add latest obstacle list to caching obstacle list
         obstacles.append(obs_list)
+
+        ### Temporary second set of data for testing
+        with open('test-uas-telem2.json') as f:
+            obs_list = json.load(f)
+
+        obstacles.append(obs_list)
+        print(obstacles)
+        ### Testing code ends here
 
         # Remove the first cached entry if the size is greater than 5
         if len(obstacles) > 5:
@@ -42,19 +50,19 @@ def responseHandler():
         else:
             return {'reroute': reroute}
 
-def create_obstacle_list(data = None):
+def create_obstacle_list(): # add back "data = None"
 
     # Code for extracting obstacles from request data
-    if data is not None:
+    '''if data is not None:
         print("Getting aircraft telemetry from gcom-x")
-        obs_list = json.load(data)
+        obs_list = json.load(data)'''
 
     # Grab the data from the test json file and add it to the objects list
     with open('test-uas-telem.json') as f:
         obs_list = json.load(f)
 
     # Print the list to see that it worked
-    print(obs_list)
+    #print(obs_list)
 
     return obs_list
 
@@ -68,7 +76,7 @@ def need_reroute(obstacles):
 
     
     # Add only drones that are in the air to the temporary list
-    for drone in obstacles:
+    for drone in obstacles[len(obstacles) - 1]:
         if drone['team']['name'] == 'UBCUAS':
             uas_drone = drone
         elif drone['inAir'] == True:
@@ -79,26 +87,20 @@ def need_reroute(obstacles):
     #print("Other aircraft:", temp_obs, "\n")
 
     x1, y1 = ll_to_utm(uas_drone['telemetry']['longitude'], uas_drone['telemetry']['latitude'])
-    s1 = calc_speed(None) # TODO: change when func is implemented
+    s1 = calc_speed(uas_drone) # TODO: change when func is implemented
     r1 = s1 * BUFFER_TIME
 
     # Compare telemetry of our aircraft to others
     for drone in temp_obs:
         
         x2, y2 = ll_to_utm(drone['telemetry']['longitude'], drone['telemetry']['latitude'])
-        s2 = calc_speed(None) # TODO: change when func is implemented
+        s2 = calc_speed(drone) # TODO: change when func is implemented
         r2 = s2 * BUFFER_TIME
-        travelled_m = 0
 
         # only add to obstacle list if in collision zone
         if collision(x1, y1, x2, y2, r1, r2):
             # Call function which formats objects for gcom-x
-            gcom_obs.append(obstacles_for_gcom(temp_obs, r2))
-
-    return True, gcom_obs
-
-def obstacles_for_gcom(temp_obs, distance):
-    gcom_obs = []
+            gcom_obs.append(obstacles_for_gcom(drone, r2))
 
     return True, gcom_obs
 
@@ -119,9 +121,9 @@ def utm_to_ll(x, y):
     global utm_meta
     return reversed(to_latlon(x, y, *utm_meta))
 
-def obstacles_for_gcomx(obs, radius):
+def obstacles_for_gcom(obs, radius):
     """ format obstacle for gcom """
-    ret = {'latitude': obs['telemetry']['latitude'], 'longitude': obs['telemetry']['longitude'], 'radius': radius, 'height':  obs['telemetry']['altitude']}
+    ret = {'latitude': obs['telemetry']['latitude'], 'longitude': obs['telemetry']['longitude'], 'radius': radius, 'height': obs['telemetry']['altitude']}
     print(ret)
     return ret
 
@@ -160,8 +162,21 @@ def calc_speed(obs):
     # Calculate total distance travelled
     for i in range(1, len(lat_history)):
         total_distance += sqrt((lat_history[i] - lat_history[i - 1]) ** 2 + (long_history[i] - long_history[i - 1]) ** 2)
+        print("Total distance: ", total_distance)
 
     # Calculate total time elapsed
+    for i in range(1, len(timestamp_history)):
+        time1 = dateutil.parser.isoparse(timestamp_history[i - 1])
+        time2 = dateutil.parser.isoparse(timestamp_history[i])
+
+        time_elapsed += (time2-time1).total_seconds()
+        print("Time elapsed: ", time_elapsed)
+    
+    # Calculate speed
+    if time_elapsed != 0:
+        drone_speed = total_distance / time_elapsed
+    
+    print("Drone speed:", drone_speed)
 
     return drone_speed
 
@@ -170,7 +185,7 @@ def collision(x1, y1, x2, y2, r1, r2):
     # Calculate 2D distance using coordinates
 
     distance = sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
-    print(distance)
+    print("Distance between: ", distance)
 
     # check if within buffer radius / collision zone
     if distance <= (r1 + r2): 
